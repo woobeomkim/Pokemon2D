11a3813828cc5986614663014ea63fc5854204fb
woobeomkim <woobum72@gmail.com>
2025-03-04 04:08:59 +0900

    BattleUI 설정, BattleSystem , BattleHud, HBBar, BattleUnit 클래스 설계

---
+++ Assets/Scripts/Battle/HPBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField] GameObject health;
+
+    public void SetHP(float normalizedHP)
+    {
+        health.transform.localScale = new Vector3(normalizedHP,1.0f);
+    }
+}
