b154ceba7133523d83a68416b2a02fa1d3b35179
woobeomkim <woobum72@gmail.com>
2025-03-08 15:40:08 +0900

    NPC추가, ProjectSetting -> TransParency Sort Mode변경 (y축이 클수록 늦게 렌더링됨), NPC와 상호작용구현, Interface를 활용해 상호작용구현

---
+++ Assets/Scripts/Character/NPCController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        Debug.Log("NPC Interactable");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
